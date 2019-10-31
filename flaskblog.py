8969f24b356e58cc370cf05cff7058cee6c54c9e
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-10-31 16:07:47 +0100

    add login and register forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, escape, request, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '74fcfe247ce3a623afb8affb76662531'
 
 posts = [
     {
@@ -31,6 +34,26 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'kchidinmavictoria@gmail.com' and form.password.data == 'password':
+            flash('Welcome back { form.username.data }! You are now logged in.', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
