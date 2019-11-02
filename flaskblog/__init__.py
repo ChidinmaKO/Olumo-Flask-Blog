8c10135ee481de9657830590260a28e13c175789
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-11-02 22:48:17 +0100

    add user authentication
    - User authentication using flask_bcrypt and flask_login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '74fcfe247ce3a623afb8affb76662531'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# database instance
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager =  LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,6 +10,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     # recaptcha = RecaptchaField('Recaptcha')
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(f"'{username.data}' is taken. Please choose a different username.")
+
+    def validate_email(self, email):
+        user_email = User.query.filter_by(email=email.data).first()
+        if user_email:
+            raise ValidationError(f"'{email.data}' exists already. Please choose a different email.")
 
 
 class LoginForm(FlaskForm):
