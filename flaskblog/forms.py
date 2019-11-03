8b16c0b92b9622723a72849464e3699c35ef6663
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-11-03 23:32:05 +0100

    add create, update and delete functionalities

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_login import current_user
 from flask_wtf import FlaskForm, RecaptchaField
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from flaskblog.models import User
@@ -49,3 +49,9 @@ class UpdateAccountForm(FlaskForm):
             user_email = User.query.filter_by(email=email.data).first()
             if user_email:
                 raise ValidationError(f"'{email.data}' exists already. Please choose a different email.")
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
